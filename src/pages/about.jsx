61a76720b325c2b5d8f4592962ab1eb52534fc5a
abhu-A-J <abhushanadhikarijoshi@gmail.com>
2021-08-20 23:12:23 +0530

    Move the dyanmic image in about page to get feom page query

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -1,21 +1,21 @@
 import * as React from 'react';
-import { Link, graphql, useStaticQuery } from 'gatsby';
+import { Link, graphql } from 'gatsby';
 import { GatsbyImage, getImage } from 'gatsby-plugin-image';
 
 /* Components */
 import Layout from '../components/Layout';
 
-export default function AboutPage() {
-  const data = useStaticQuery(graphql`
-    query cockTailImage {
-      file(name: { eq: "cocktail" }) {
-        childImageSharp {
-          gatsbyImageData(placeholder: BLURRED)
-        }
+export const query = graphql`
+  query cockTailImage {
+    file(name: { eq: "cocktail" }) {
+      childImageSharp {
+        gatsbyImageData(placeholder: BLURRED)
       }
     }
-  `);
+  }
+`;
 
+export default function AboutPage({ data }) {
   const image = getImage(data.file);
   return (
     <>
