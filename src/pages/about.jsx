2392372831b0a8162ddf4efd1887301c9bdcc80e
abhu-A-J <abhushanadhikarijoshi@gmail.com>
2021-08-20 23:05:41 +0530

    Querying images from the gatsby graphql

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -1,13 +1,26 @@
 import * as React from 'react';
-import { Link } from 'gatsby';
+import { Link, graphql, useStaticQuery } from 'gatsby';
+import { GatsbyImage, getImage } from 'gatsby-plugin-image';
 
 /* Components */
 import Layout from '../components/Layout';
 
 export default function AboutPage() {
+  const data = useStaticQuery(graphql`
+    query cockTailImage {
+      file(name: { eq: "cocktail" }) {
+        childImageSharp {
+          gatsbyImageData(placeholder: BLURRED)
+        }
+      }
+    }
+  `);
+
+  const image = getImage(data.file);
   return (
     <>
       <Layout title="About" description="Learniong Gatsby about page">
+        <GatsbyImage image={image} alt="Alternative text for image" />
         <h1>About page</h1>
         <Link to="/">Back to home page</Link>
       </Layout>
