3854e9184fdf14b83442bd571f65ecf4dc108ce1
abhu-A-J <abhushanadhikarijoshi@gmail.com>
2021-08-20 22:18:40 +0530

    Adding static images into the project

--- gatsby-config.js
+++ gatsby-config.js
@@ -37,5 +37,17 @@ module.exports = {
         },
       },
     },
+
+    // plugins to setup image
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        name: 'images',
+        path: `${__dirname}/src/images/`,
+      },
+    },
+    'gatsby-plugin-image',
+    'gatsby-plugin-sharp',
+    'gatsby-transformer-sharp',
   ],
 };
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,8 +1,12 @@
 import * as React from 'react';
 import { Link, graphql, useStaticQuery } from 'gatsby';
+import { StaticImage } from 'gatsby-plugin-image';
 
 /* Components */
 import Layout from '../components/Layout';
+
+/* Styling */
+import { imageWrapper } from '../styles/index.module.css';
 
 export default function IndexPage() {
   const data = useStaticQuery(graphql`
@@ -25,6 +29,15 @@ export default function IndexPage() {
   return (
     <>
       <Layout>
+        <div clasName={imageWrapper}>
+          <StaticImage
+            src="../images/ivana-la-61jg6zviI7I-unsplash.jpg"
+            alt="Alternate text for the image"
+            width={300}
+            height={300}
+            placeholder="dominantColor"
+          />
+        </div>
         <h1>Index page</h1>
         <Link to="/about">About page</Link>
         <h2>Check all my recent blogs</h2>
